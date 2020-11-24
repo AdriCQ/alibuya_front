8e0db3b9992a8b501ac21637437d767f0d907429
DCQ <royal.boxx@nauta.cu>
2020-11-23 19:51:23 -0500

    Create Auth view, route and layout

---
+++ src/components/layouts/AuthLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="fill-height py-6">
+    <v-container class="mx-auto">
+      <v-row justify="center">
+        <v-col cols="auto">
+          <v-img
+            src="img/logos/logo_550x178.png"
+            class="mx-auto"
+            max-width="300"
+          />
+          <router-view />
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
+</template>
+
+<script lang='ts'>
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class AuthLayout extends Vue {}
+</script>
